62d976bd5c7e6d1d3ff7f4e7d635b017adf86bc5
NickLegacy <nicklegacy13@gmail.com>
2022-09-23 22:04:18 -0400

    initial commit

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Bird : Animal
+    {
+        public int Wings { get; set; }
+        public string Type { get; set; }
+        public bool IsWarmBlooded { get; set; }
+        public bool HasFeathers { get; set; }
+
+        public Bird()
+        {
+
+        }
+
+        public Bird(string name, int age, int legs, bool hasBackbone, int wings, string type, bool isWarmBlooded, bool hasFeathers)
+        {
+            Name = name;
+            Age = age;
+            Legs = legs;
+            HasBackbone = hasBackbone;
+            Wings = wings;
+            Type = type;
+            IsWarmBlooded = isWarmBlooded;
+            HasFeathers = hasFeathers;
+        }
+
+     
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -16,6 +16,7 @@ namespace Inheritance
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
+
             // Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
@@ -28,12 +29,43 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird Birdy = new Bird()
+            {
+                Name = "Big Bird",
+                Age = 50,
+                Legs = 2,
+                HasBackbone = true,
+                IsWarmBlooded = true,
+                HasFeathers = true,
+                Wings = 2,
+                Type = "Yellow",
+            };
+
+            Console.WriteLine($"{Birdy.Name} is {Birdy.Age} years old. He has {Birdy.Legs} legs and also {Birdy.Wings} wings. He has a backbone," +
+            $" he is warmblooded and also has feathers that are {Birdy.Type}.");
+
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
-        }
-    }
+            Reptile dragon = new Reptile()
+            {
+                Name = "Fire Dragon",
+                Age = 1200,
+                Legs = 4,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Environment = "Fire Relm",
+                Defense = "breath fire"
+            };
+
+            Console.WriteLine($"The {dragon.Name} has lived for over {dragon.Age} years. He has {dragon.Legs} legs, a backbone, and scales." +
+            $" Even though he is cold blooded, he is able to {dragon.Defense}. He lives in the {dragon.Environment}."); 
+
+}
+          
+}
 }
